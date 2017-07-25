bc5d905987dd7eb9667fa60365fecc4b9a4b081f
fulilan <lanfuli007@gmail.com>
2017-07-25 01:43:46 -0700

    [Add] Query Set Display
    Query set
    for loop
    context

--- ebdjango/posts/views.py
+++ ebdjango/posts/views.py
@@ -2,11 +2,28 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def post_home(request):
-    return render(request, "index.html", {})
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My user list"
+    # }
+    # #     here you can use different render to show different html page return render(request, "index.html", context)
+    # else:
+    #     context = {
+    #         "title": "General list"
+    #     }
+    # return render(request, "index.html", context)
+    queryset = Post.objects.all()
+    context = {
+        "query_list": queryset,
+        "title": "General list"
+        }
+    return render(request, "index.html", context)
+
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 def post_detail(request):
