538f96622634c56b2952757bb1444973ea265186
fulilan <lanfuli007@gmail.com>
2017-07-25 02:25:49 -0700

    [Add] 404 error page
    get_object_or_404 not list

--- ebdjango/posts/views.py
+++ ebdjango/posts/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -27,7 +27,12 @@ def post_home(request):
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, id=1)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        }
+    return render(request, "post_detail.html", context)
 def post_update(request):
     return HttpResponse("<h1>update</h1>")
 def post_delete(request):
